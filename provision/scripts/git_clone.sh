6374d44dcbe66a4575ceb0f057b351973e2ad1ab
ArtificialAmateur <me@artificialamateur.xyz>
2019-01-25 21:20:37 -0600

    Major restructure

--- provision/scripts/git_clone.sh
+++
@@ -1,6 +0,0 @@
-if [ -d $2 ]; then
-	echo "The git repo already exists in the guest machine. If you are reprovisioning, do a git pull from within the VM or remove the directory and try again."
-	exit
-fi
-
-rm -rf ~/MES/ && mkdir ~/MES && git clone $1 $2
