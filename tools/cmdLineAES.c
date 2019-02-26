c576d5a3dc03e2b9d7af3169aa03ff697eb4efbe
KendrickU <kurbaniak@unomaha.edu>
2019-02-25 20:46:09 -0600

    probably should write to that file

--- tools/cmdLineAES.c
+++ tools/cmdLineAES.c
@@ -50,7 +50,7 @@ int main(int argc, char *const argv[])
   // Open the file for write binary mode
   FILE * enc_game = fopen(encrypted_game, "wb");
   // Write to the file
-  enc_game.fwrite(game_buffer, game_size, sizeof(char), enc_game);
+  fwrite(game_buffer, game_size, sizeof(char), enc_game);
   // End of function
   return 0;
 }
